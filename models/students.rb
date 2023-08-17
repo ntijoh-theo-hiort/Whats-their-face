39ac2b3e2063231891b28e6ef1437c5bffa8166a
DanisMusicNTI <danis.music@elev.ga.ntig.se>
2023-08-17 15:39:11 +0200

    added some functions

---
+++ models/students.rb
@@ -0,0 +1,23 @@
+class Students
+
+    def self.all
+        db.execute("SELECT * FROM students")
+    end
+
+    def self.fullname
+        db.execute("SELECT name FROM students")
+    end
+
+    def self.name_from_id(id)
+        db.execute("SELECT name FROM students WHERE id=?", id)
+    end
+
+
+    private 
+    def self.db 
+        return @db if @db   #return @db if it exists XD
+        @db = SQLite3::Database.new("db/students.db")
+        @db.results_as_hash = true
+        @db
+    end
+end
