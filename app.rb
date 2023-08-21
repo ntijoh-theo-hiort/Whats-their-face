22b16822a6e4c26482cc6fed5b4d5d0684951074
theo-hiort-nti-johanneberg <theo.hiort@elev.ga.ntig.se>
2023-08-21 09:10:29 +0200

    getting the quiz started
    you now go to the quiz_start page to start the quiz and are then taken to quiz/:game_id to play the quiz
    
    new methods created in model.rb related to starting a new game
    
    some methods returning more accurately example: [[5]] -> 5
    
    "/" start page created

--- app.rb
+++ app.rb
@@ -1,21 +1,42 @@
 class App < Sinatra::Base
+    enable :sessions
 
 
-    get '/quiz' do
+    get '/' do
+        erb :start
+    end
+
+    get '/quiz_start' do
+        erb :quiz_start
+    end
+
+    get '/quiz/:game_id' do
+        @id = Students.random_student_id_from_game(params[:game_id])
+        @full_name = Students.name_from_id(@id)
         erb :quiz
     end
 
     get '/cheatsheet' do
-        @ids = Students.all_ids
+        @students = Students.all
         erb :cheatsheet
     end
 
-    get '/new-entry' do
+    get '/new_entry' do
         erb :new_entry
     end 
 
     post '/quiz' do
-        
+        #check if answer was correct:
+        #if correct, set guessed? in database to True
+        #also set session[:last_guess] to True
+        #else set session[:last_guess] to False
+        redirect("/quiz/#{@game_id}")
     end
 
+    post '/quiz_start' do
+        @students = Students.all
+        @game_id = Students.create_new_game(@students.count)
+        session[:last_guess] = nil
+        redirect("/quiz/#{@game_id}")
+    end
 end
--- models/students.rb
+++ models/students.rb
@@ -1,26 +1,52 @@
 class Students
 
     def self.all
-        db.execute("SELECT * FROM students")
+        db.execute('SELECT * FROM students')
     end
 
     def self.all_ids
-        db.execute('SELECT id FROM students').map{|item| item["id"]}
+        db.execute('SELECT id FROM students').map{|item| item['id']}
     end
 
     def self.fullname
-        db.execute("SELECT name FROM students")
+        db.execute('SELECT full_name FROM students').map{|item| item['name']}
     end
 
     def self.name_from_id(id)
-        db.execute("SELECT name FROM students WHERE id=?", id)
+        db.results_as_hash = false
+        db.execute('SELECT full_name FROM students WHERE id=?', id)[0][0]
     end
 
+    def self.create_new_game(student_amount)
+        new_id = db.execute('SELECT MAX(id) FROM games')[0]["MAX(id)"]
+
+        if !new_id
+            new_id = 1
+        else
+            new_id += 1
+        end
+
+        i = 1
+        until i > student_amount
+            db.execute('INSERT INTO games (id, student_id, guessed) VALUES (?,?,?)', new_id, i, 'False')
+            i += 1
+        end
+
+        new_id
+    end
+
+    def random_student_id_from_game(game_id)
+        # get a random id from the 'games' table where guessed == "False"
+    end
+
+    def set_guessed_to_false(student_id)
+        # set guessed to false in 'games' table where id matches argument for this method
+    end
 
     private 
     def self.db 
         return @db if @db   #return @db if it exists XD
-        @db = SQLite3::Database.new("db/students.db")
+        @db = SQLite3::Database.new('db/students.db')
         @db.results_as_hash = true
         @db
     end
